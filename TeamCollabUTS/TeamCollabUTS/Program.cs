a81d6d283b377309d417cc4957cbe0e3ad3bb212
Griselda Guinarto <griseldaguinarto1998@gmail.com>
2018-07-06 19:37:26 +0700

    Jawaban UTS

---
+++ TeamCollabUTS/TeamCollabUTS/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCollabUTS
+{
+    public class HitungKelipatan
+    {
+        static void Main(string[] args)
+        {
+            int x, n;
+
+            Console.Write("Masukan X : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Masukan N : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            hitungKelipatan(x, n);
+        }
+
+        public static void hitungKelipatan(int x, int n)
+        {
+            int total = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (x == 0)
+                {
+                    total = 0;
+                }
+                else
+                {
+                    while (i % x == 0)
+                    {
+                        total += i;
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total adalah " + total);
+        }
+    }
+}
